0f288e65679b44a1fbcc553f9160df8e4521219b
Letícia Barbosa Silva <leticiabarbosasilva94@gmail.com>
2019-12-04 09:54:33 -0300

    Added styled components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+import Header from './components/Header';
+
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes />
+      <GlobalStyles />
+    </BrowserRouter>
+  );
 }
 
 export default App;
