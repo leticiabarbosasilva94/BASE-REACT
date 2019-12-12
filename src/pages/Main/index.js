e7afb37857200328d83ac9ca6db3cd2b4629798a
Letícia Barbosa Silva <leticiabarbosasilva94@gmail.com>
2019-12-12 13:35:09 -0300

    Added redux saga

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 
+// Saga
+import * as exampleRequest from '../../store/modules/exampleModule/actions';
+
 export default function Main() {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(exampleRequest.exampleActionRequest('HEYYYYYYYYYYY'));
+  }, [dispatch]);
+
   toast.success('Pretty cool message!!!');
   return <h1>Main</h1>;
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
 
-const store = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  compose(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
